14bcb2c02266e8f45760bf24398c4760f60fd11b
macamp0328 <macamp0328@gmail.com>
2023-09-10 15:01:58 -0500

    cdk added

---
+++ test/die_young_stay_bright.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DieYoungStayBright from '../lib/die_young_stay_bright-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/die_young_stay_bright-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DieYoungStayBright.DieYoungStayBrightStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
